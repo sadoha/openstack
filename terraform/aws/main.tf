a2279e573b4075d3dcb5498a19854773c0d67cf7
Andrii Sadovskyi <andrii.sadovskyi@gmail.com>
2025-10-03 10:15:07 +0300

    Add tls private key

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -150,9 +150,23 @@ resource "aws_security_group" "private_sg" {
   }
 }
 
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.ssh.private_key_pem
+  filename = "ssh-keys/private_ssh_key"
+}
+
+output "public_ssh_key" {
+  value = tls_private_key.ssh.public_key_openssh
+}
+
 resource "aws_key_pair" "main" {
   key_name   = "openstack"
-  public_key = file("ssh-keys/openstack.pub")
+  public_key = tls_private_key.ssh.public_key_openssh
 }
 
 resource "aws_instance" "jumphost" {
