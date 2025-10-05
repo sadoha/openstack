4bae0d34553d271d0b0881aaece64a32e4f6b8f0
Andrii Sadovskyi <andrii.sadovskyi@gmail.com>
2025-10-05 23:26:13 +0300

    Update Ansible configuration

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -197,7 +197,7 @@ resource "aws_instance" "jumphost" {
 
 resource "aws_instance" "controller" {
   ami                         = "ami-0360c520857e3138f"
-  instance_type               = "t2.medium"
+  instance_type               = "t2.large"
   key_name                    = aws_key_pair.main.key_name
   private_ip                  = "10.0.1.11"
   subnet_id                   = aws_subnet.private_subnet[0].id
